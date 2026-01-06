17d678cbec68065164457940847330d2d396d06c
Junaid Khan <jk7466809@gmail.com>
2026-01-06 15:35:23 +0530

    intial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+//middleware
+app.use(express.json());
+
+//test route
+app.get('/', (req,res) => {
+    res.send("API is running");
+});
+
+module.exports = app;
