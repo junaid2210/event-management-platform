843b6e8bb7b789916f41569a5ab0c83ef4d0dd3e
Junaid Khan <jk7466809@gmail.com>
2026-01-06 16:02:49 +0530

    Add User model

---
+++ src/models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim:true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+
+        passwordHash: {
+            type: String,
+            required: true
+        },
+
+        role: {
+            type: String,
+            enum: ['studend', 'organiser'],
+            required: true
+        },
+
+        collegeId: {
+            type: String,
+            required: true
+        }
+    },
+
+    {timestamps: true}
+);
+
+module.exports = mongoose.model('User',userSchema);
