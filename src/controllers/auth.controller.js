dee82cb0ecb62dc094b9c7a35217742bab0c7ebe
Junaid Khan <jk7466809@gmail.com>
2026-01-07 12:36:11 +0530

    Add login and JWT authentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/User.model');
+const generateToken = require('../utils/generatetokens');
 
 const register = async (req, res) => {
     try{
@@ -44,4 +45,40 @@ const register = async (req, res) => {
     }
 };
 
-module.exports = {register};
+const login = async (req,res) => {
+    try{
+        const {email , password} = req.body;
+
+        if(!email || !password) {
+            return res.status(400).json({message:'Email and password are required'});
+        }
+
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({message:'Invalid credentials'});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.passwordHash);
+        if(!isMatch){
+            return res.status(401).json({message:'Invalid credentials'});
+        }
+
+        const token = generateToken(user);
+        
+        res.json({
+            message:'Login successful',
+            token,
+            user:{
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+        })
+    }catch(err){
+        console.error(error);
+        res.status(500).json({message:'Server error'});
+    }
+};
+
+module.exports = {register, login};
