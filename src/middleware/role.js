4d69a8b30c050d316f0b865d3b57fec2982a8d29
Junaid Khan <jk7466809@gmail.com>
2026-01-08 19:42:31 +0530

    Fix role middleware to stop execution on unauthorized access

--- src/middleware/role.js
+++ src/middleware/role.js
@@ -9,6 +9,7 @@ const isStudent = (req, res, next) => {
     if(req.user.role !== 'student'){
         return res.status(403).json({message: 'Student access only'});
     }
+    next();
 };
 
 module.exports = {isOrganizer, isStudent};
