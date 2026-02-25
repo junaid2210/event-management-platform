90ae45b317d6a389180479073a1a8a74f3515df1
Junaid Khan <jk7466809@gmail.com>
2026-02-25 22:38:57 +0530

    minor fixes

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/auth', limiter);
 app.use(cors({
-    origin: 'https://localhost:5173',
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-type', 'Authorization']
