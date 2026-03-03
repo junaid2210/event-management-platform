f8811f9c2d13a4ca8d0ac0b3f50edc25f35d1d56
Junaid Khan <jk7466809@gmail.com>
2026-03-03 10:50:16 +0530

    added guestroute to restrict access to login/register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,34 @@
 import './App.css'
 import Navbar from './components/navbar';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, useLocation} from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import GuestRoute from './components/GuestRoute';
 
 const TempHome = () => <div className="p-10">Welcome to JECRC Events!</div>;
 
 function App() {
+  const location = useLocation();
+
+  const authPaths = ['/login','/register'];
+  const showNavbar = !authPaths.includes(location.pathname);
+
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navbar/>
+      {showNavbar && <Navbar/>}
       <main>
         <Routes>
           <Route path="/" element={<TempHome />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={
+            <GuestRoute>
+              <Login />
+            </GuestRoute>
+            } />
+          <Route path="/register" element={
+            <GuestRoute>
+              <Register />
+            </GuestRoute>
+            } />
         </Routes>
       </main>
     </div>
